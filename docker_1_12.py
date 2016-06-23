85adc6d5724fcf8728be765f3300620380b3d007
Ian Miell <ian.miell@gmail.com>
2016-06-23 08:00:59 +0100

    latest

--- docker_1_12.py
+++ docker_1_12.py
@@ -1,77 +1,9 @@
-"""ShutIt module. See http://shutit.tk
-"""
-
 from shutit_module import ShutItModule
-
+import time
 
 class docker_1_12(ShutItModule):
 
-
 	def build(self, shutit):
-		# Some useful API calls for reference. See shutit's docs for more info and options:
-		#
-		# ISSUING BASH COMMANDS
-		# shutit.send(send,expect=<default>) - Send a command, wait for expect (string or compiled regexp)
-		#                                      to be seen before continuing. By default this is managed
-		#                                      by ShutIt with shell prompts.
-		# shutit.multisend(send,send_dict)   - Send a command, dict contains {expect1:response1,expect2:response2,...}
-		# shutit.send_and_get_output(send)   - Returns the output of the sent command
-		# shutit.send_and_match_output(send, matches) 
-		#                                    - Returns True if any lines in output match any of 
-		#                                      the regexp strings in the matches list
-		# shutit.send_until(send,regexps)    - Send command over and over until one of the regexps seen in the output.
-		# shutit.run_script(script)          - Run the passed-in string as a script
-		# shutit.install(package)            - Install a package
-		# shutit.remove(package)             - Remove a package
-		# shutit.login(user='root', command='su -')
-		#                                    - Log user in with given command, and set up prompt and expects.
-		#                                      Use this if your env (or more specifically, prompt) changes at all,
-		#                                      eg reboot, bash, ssh
-		# shutit.logout(command='exit')      - Clean up from a login.
-		# 
-		# COMMAND HELPER FUNCTIONS
-		# shutit.add_to_bashrc(line)         - Add a line to bashrc
-		# shutit.get_url(fname, locations)   - Get a file via url from locations specified in a list
-		# shutit.get_ip_address()            - Returns the ip address of the target
-		# shutit.command_available(command)  - Returns true if the command is available to run
-		#
-		# LOGGING AND DEBUG
-		# shutit.log(msg,add_final_message=False) -
-		#                                      Send a message to the log. add_final_message adds message to
-		#                                      output at end of build
-		# shutit.pause_point(msg='')         - Give control of the terminal to the user
-		# shutit.step_through(msg='')        - Give control to the user and allow them to step through commands
-		#
-		# SENDING FILES/TEXT
-		# shutit.send_file(path, contents)   - Send file to path on target with given contents as a string
-		# shutit.send_host_file(path, hostfilepath)
-		#                                    - Send file from host machine to path on the target
-		# shutit.send_host_dir(path, hostfilepath)
-		#                                    - Send directory and contents to path on the target
-		# shutit.insert_text(text, fname, pattern)
-		#                                    - Insert text into file fname after the first occurrence of 
-		#                                      regexp pattern.
-		# shutit.delete_text(text, fname, pattern)
-		#                                    - Delete text from file fname after the first occurrence of
-		#                                      regexp pattern.
-		# shutit.replace_text(text, fname, pattern)
-		#                                    - Replace text from file fname after the first occurrence of
-		#                                      regexp pattern.
-		# ENVIRONMENT QUERYING
-		# shutit.host_file_exists(filename, directory=False)
-		#                                    - Returns True if file exists on host
-		# shutit.file_exists(filename, directory=False)
-		#                                    - Returns True if file exists on target
-		# shutit.user_exists(user)           - Returns True if the user exists on the target
-		# shutit.package_installed(package)  - Returns True if the package exists on the target
-		# shutit.set_password(password, user='')
-		#                                    - Set password for a given user on target
-		#
-		# USER INTERACTION
-		# shutit.get_input(msg,default,valid[],boolean?,ispass?)
-		#                                    - Get input from user and return output
-		# shutit.fail(msg)                   - Fail the program and exit with status 1
-		# 
 		shutit.send('rm -rf /tmp/docker1_12')
 		box = shutit.send_and_get_output('vagrant box list 2>/dev/null | grep ubuntu/xenial64')
 		if box == '':
@@ -86,14 +18,18 @@ class docker_1_12(ShutItModule):
 		shutit.send('curl -sSL -O https://experimental.docker.com/builds/Linux/x86_64/docker-latest.tgz && tar -zxvf docker-latest.tgz && cd docker')
 		shutit.send('mv * /usr/bin')
 		shutit.send('cd')
-		shutit.send('dockerd &')
+		shutit.send('dockerd > /dev/null 2>&1 &')
+
+		time.sleep(5)
+		shutit.send('docker version',note='Confirm version of docker as 1.12')
 
 		# Log into docker
-		shutit.multisend('docker login',{'sername':shutit.cfg[self.module_id]['docker_username'],'assword':shutit.cfg[self.module_id]['docker_password']})
+		shutit.send('docker login',expect='sername')
+		shutit.send(shutit.cfg[self.module_id]['docker_username'],expect='assword')
+		shutit.send(shutit.cfg[self.module_id]['docker_password'])
 
-#DOCKERFILE 
-#HEALTHCHECK --interval=5m --grace=20s --timeout=3s --exit-on-unhealthy CMD curl -f http://localhost/
-		shutit.send('mkdir hello && cd hello')
+		shutit.send('mkdir hello')
+		shutit.send('cd hello')
 		shutit.send_file('hello.py','''#!/usr/bin/env python
 import os
 import socket
@@ -124,36 +60,38 @@ RUN mkdir /www
 ADD hello.py /www/
 RUN chmod +x /www/hello.py
 WORKDIR /www
-HEALTHCHECK --interval=10s --timeout=3s --retries=1 CMD curl localhost:8080
+HEALTHCHECK --interval=10s --timeout=3s --retries=1 CMD wget -O /tmp/wget localhost:80
+EXPOSE 80
 CMD ["python", "hello.py"]''')
 		shutit.send('docker build -t imiell/hello .')
-		shutit.send('docker push imiell/hello')
+		#shutit.send('docker push imiell/hello')
 
-		shutit.send('docker version',note='Confirm version of docker as 1.12')
-#Usage:	docker swarm COMMAND
-#  join        Join a Swarm as a node and/or manager
-#  update      Update the Swarm
-#  leave       Leave a Swarm
+		#Usage:	docker swarm COMMAND
+		#  join        Join a Swarm as a node and/or manager
+		#  update      Update the Swarm
+		#  leave       Leave a Swarm
 		shutit.send('docker swarm init',note='Initialise a swarm')
 		shutit.send('docker swarm inspect',note='Inspect the swarm')
 
-#Usage:	docker node COMMAND
-#  accept      Accept a node in the swarm
-#  demote      Demote a node from manager in the swarm
-#  inspect     Inspect a node in the swarm
-#  promote     Promote a node to a manager in the swarm
-#  rm          Remove a node from the swarm
-#  tasks       List tasks running on a node
-#  update      Update a node
+		#Usage:	docker node COMMAND
 		shutit.send('docker node ls',note='List the nodes in the swarm')
+		shutit.send('docker node inspect ubuntu-xenial',note='Inspect the node')
 
-#Usage:	docker service COMMAND
-#  tasks       List the tasks of a service
-#  rm          Remove a service
-#  scale       Scale one or multiple services
-#  update      Update a service
-		shutit.send('docker service create imiell/hello',note='Create a simple echo service')
+		#Usage:	docker service COMMAND
+		shutit.send('docker service create imiell/hello',note='Create a simple hello service')
 		shutit.send('docker service ls',note='List the services in the swarm')
+		service_id = shutit.send_and_get_output("""docker service ls | tail -1 | awk '{print $1}'""")
+		shutit.send("""docker service inspect """ + service_id,note='Inspect the service')
+		shutit.send("""docker service scale """ + service_id + """=2""",note='Scale the service to two containers')
+		shutit.send("""docker service inspect """ + service_id,note='Inspect the service, note it now has 2 replicas')
+		shutit.send('docker node tasks self',note='List the tasks running on this node.')
+		shutit.send('docker node update --availability=pause ubuntu-xenial',note='')
+		shutit.send('docker node tasks self',note='')
+		shutit.send('docker node update --availability=active ubuntu-xenial',note='')
+		shutit.send('docker service rm ' + service_id,note='Remove the hello service')
+		shutit.send('docker service ls',note='No services remain')
+
+# TODO: (experimental) New stack and deploy commands to manage and deploy multi-service applications #23522
 		shutit.pause_point('')
 		shutit.logout()
 		return True
@@ -166,7 +104,7 @@ CMD ["python", "hello.py"]''')
 		# shutit.get_config(self.module_id, 'myconfig', default='a value')
 		#                                      and reference in your code with:
 		# shutit.cfg[self.module_id]['myconfig']
-		shutit.get_config(self.module_id, 'docker_username',secret=True)
+		shutit.get_config(self.module_id, 'docker_username',default='imiell')
 		shutit.get_config(self.module_id, 'docker_password',secret=True)
 		return True
 
